d6455965f6841005abec27f380503248e7476ced
Boobielord <110482677+Boobielord@users.noreply.github.com>
2025-04-07 11:19:13 +0200

    dd

--- Madspild/src/main/java/com/example/madspild/Controller/LoginController.java
+++ Madspild/src/main/java/com/example/madspild/Controller/LoginController.java
@@ -1,4 +1,15 @@
 package com.example.madspild.Controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
 public class LoginController {
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @PostMapping
 }
---
+++ Madspild/src/main/java/com/example/madspild/Service/LoginService.java
@@ -0,0 +1,4 @@
+package com.example.madspild.Service;
+
+public class LoginService {
+}
